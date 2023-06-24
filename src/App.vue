c6745a8b56533ccfcecd89699e3e19f10bde9d0e
z80b <akozlov78@gmail.com>
2023-06-24 13:10:09 +0300

    add proxy config and base components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div
+      v-for="{ id, title, image } in products"
+      :key="`product-${id}`"
+    >
+      <span>{{ title }}</span>
+      <img :src="image"/>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+type Product = {
+  title: string,
+  image: string,
+  price: number,
+};
+
+const products = ref<Product[]>();
+
+onMounted(async () => {
+  const response = await fetch('/api/products');
+  products.value = await response.json();
+});
+</script>
+
+
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api/products': 'https://my-json-server.typicode.com/z80b/pizza/products',
+      '^/api/product/.*': {
+        target: 'https://my-json-server.typicode.com/z80b/pizza/products',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/product\//, ''),
+      },
+    },
+  },
 })
